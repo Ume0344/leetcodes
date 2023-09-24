d2195019fff8cb2b93e972ce5a44e90671ccd08d
Ume0344 <uhabiba005@gmail.com>
2023-09-25 00:14:14 +0200

    Add implementation for breadth first values

--- binary_tree/binary_tree.py
+++ binary_tree/binary_tree.py
@@ -1,9 +1,12 @@
 from typing import List
+
+
 class Node:
     def __init__(self, data) -> None:
         self.data: Any = data
         self.right : Node = None
         self.left: Node = None
+
 
 def create_binary_node() -> Node:
     a = Node(1)
@@ -12,12 +15,26 @@ def create_binary_node() -> Node:
     d = Node(4)
     e = Node(5)
     f = Node(6)
+    g = Node(7)
+    h = Node(8)
+    i = Node(9)
+    j = Node(10)
+    k = Node(11)
+    l = Node(12)
+
 
     a.left = b
     a.right = c
     b.left = d
     b.right = e
-    c.right = f 
+    c.left = f
+    c.right = g
+    d.left = h
+    h.left = l
+    e.right = i
+    f.left = j
+    g.right = k
+
 
     """
              a
@@ -30,6 +47,7 @@ def create_binary_node() -> Node:
 
     return a
 
+
 def print_tree(root: Node) -> None:
     print(root.data)
     if root.left:
@@ -37,10 +55,12 @@ def print_tree(root: Node) -> None:
     if root.right:
         print_tree(root.right)
 
-def depth_first_value(root: Node, values_array: List) -> List:
+
+def depth_first_value_recursion(root: Node, values_array: List) -> List:
     """
-    Traverse through binary tree depthwise. Can be done by using recursion and stack.
-    Prefer to do with recursion
+    Traverse through binary tree depthwise. It can be done by using recursion and stack.
+    We prefer to do with recursion. Under the hood of recursion, we are anyways using stack
+    so same answers are expected.
 
     param root: Root of the tree
     param values_array: Array to store values of tree nodes.
@@ -50,9 +70,84 @@ def depth_first_value(root: Node, values_array: List) -> List:
         return values_array
     values_array.append(root.data)
     if root.left:
-        depth_first_value(root.left, values_array)
+        depth_first_value_recursion(root.left, values_array)
     if root.right:
-        depth_first_value(root.right, values_array)
+        depth_first_value_recursion(root.right, values_array)
     
     return values_array
 
+
+def depth_first_value_stack(root: Node) -> List:
+    """
+    Traverse through binary tree depthwise with stacks..
+
+    param root: Root of the tree
+    param values_array: Array to store values of tree nodes.
+    return list of values of binary tree nodes.
+    """
+    values_array: List = []
+    if root == None:
+        return values_array
+    
+    stack = [root]
+    while len(stack) > 0:
+        current = stack.pop()
+        values_array.append(current.data)
+
+        if current.right:
+            stack.append(current.right)
+        if current.left:
+            stack.append(current.left)
+    
+    return values_array
+
+
+def breadth_first_value(root: Node) -> List:
+    """
+    Traverse through binary tree breadth wise. Can be done by using recursion and queue.
+    Prefer to do with queue. There is no straight forward solution with recursion as resursion uses
+    stack under the hood and we need queue to solve it.
+
+    param root: Root of the tree
+    param values_array: Array to store values of tree nodes.
+    return list of values of binary tree nodes.
+    """
+    # go to root, put data into value
+    # go to root.left, put data
+    # go to root.right, put data
+    # again go to left, make it a new root and repeat the process.
+    # go to right, make it new root and repeat the process.
+    '''
+    This solution can work if tree depth is 2, after words it wont work. Need 
+    alot of checks to make it work.
+    if root == None:
+        return values_array
+    
+    if root and i == 0:
+        values_array.append(root.data)
+
+    if root.left:
+        values_array.append(root.left.data)
+
+    if root.right:
+        values_array.append(root.right.data)
+    i +=1
+    breadth_first_value(root.left, values_array, i)
+    breadth_first_value(root.right, values_array, i)
+    '''
+    values_array: List = []
+    
+    if root is None:
+        return values_array
+
+    queue = [root]
+    while len(queue) > 0:
+        current = queue.pop(0)
+        values_array.append(current.data)
+
+        if current.left:
+            queue.append(current.left)
+        if current.right:
+            queue.append(current.right)
+    
+    return values_array
--- binary_tree/test_binary_tree.py
+++ binary_tree/test_binary_tree.py
@@ -1,13 +1,27 @@
 import unittest
 import binary_tree
 
+
 class TestBinaryTree(unittest.TestCase):
-    def test_depth_first_value(self):
+    def test_depth_first_value_recursion(self):
         root = binary_tree.create_binary_node()
         values = []
-        values = binary_tree.depth_first_value(root, values)
+        values = binary_tree.depth_first_value_recursion(root, values)
 
-        assert values == [1, 2, 4, 5, 3, 6]
+        assert values == [1, 2, 4, 8, 12, 5, 9, 3, 6, 10, 7, 11]
+    
+    def test_depth_first_value_stack(self):
+        root = binary_tree.create_binary_node()
+        values = binary_tree.depth_first_value_stack(root)
+
+        assert values == [1, 2, 4, 8, 12, 5, 9, 3, 6, 10, 7, 11]
+
+    def test_breadth_first_value(self):
+        root = binary_tree.create_binary_node()
+        values = binary_tree.breadth_first_value(root)
+
+        assert values == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
     
 if __name__ == '__main__':
     unittest.main()
